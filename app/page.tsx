4711825ba53f0c77c1e85a5c2d2d39b39667d01c
Daniel Mendes Neto <danielm_netto@hotmail.com>
2025-11-13 23:00:48 -0300

    Primeiro commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,156 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { DicesIcon, RotateCcw } from "lucide-react";
+
+const MINIMO_QTDE_NUMEROS = 5;
+const MAXIMO_QTDE_NUMEROS = 300;
+const QTDE_PADRAO_NUMEROS = 75;
 
 export default function Home() {
+  const [numeroSorteado, setNumeroSorteado] = useState<number | null>(null);
+  const [numerosSorteados, setNumerosSorteados] = useState<Array<number>>([]);
+  const [novaQtdeNumeros, setNovaQtdeNumeros] =
+    useState<number>(QTDE_PADRAO_NUMEROS);
+  const [qtdeNumeros, setQtdeNumeros] = useState<number>(novaQtdeNumeros);
+
+  const pegarNumeroAleatorio = (minimo: number, maximo: number) => {
+    minimo = Math.ceil(minimo);
+    maximo = Math.floor(maximo);
+    return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+  };
+
+  const sortearNumero = () => {
+    if (numerosSorteados.length >= qtdeNumeros) {
+      return;
+    }
+
+    let numeroSorteado = 0;
+    let achouNumeroJaSorteado = true;
+
+    while (achouNumeroJaSorteado) {
+      numeroSorteado = pegarNumeroAleatorio(1, qtdeNumeros);
+
+      if (
+        !numerosSorteados.some(
+          (numeroJaSorteado) => numeroSorteado == numeroJaSorteado
+        )
+      ) {
+        achouNumeroJaSorteado = false;
+      }
+    }
+    setNumeroSorteado(numeroSorteado);
+    setNumerosSorteados([numeroSorteado, ...numerosSorteados]);
+  };
+
+  const reiniciarSorteio = () => {
+    setNumeroSorteado(null);
+    setNumerosSorteados([]);
+    setQtdeNumeros(novaQtdeNumeros || QTDE_PADRAO_NUMEROS);
+  };
+
+  const confirmarReiniciarSorteio = () => {
+    const confirmacao = confirm(
+      "Você tem certeza que deseja reiniciar o sorteio?"
+    );
+
+    if (!confirmacao) return;
+
+    reiniciarSorteio();
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <main className="relative flex flex-col">
+      <div className="flex justify-center gap-6 mx-auto my-2 px-5 py-4 border-2 border-slate-200 rounded-2xl">
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-1 md:gap-2 h-104 sm:h-128 md:h-full overflow-auto pr-3">
+          {Array.from({ length: qtdeNumeros }).map((numero, index) => {
+            const numeroJaSorteado = numerosSorteados.some(
+              (numero) => numero === index + 1
+            );
+
+            return (
+              <div
+                key={`lista-${index}-${numero}`}
+                className={cn(
+                  `flex items-center justify-center rounded-lg bg-slate-300 sm:px-4 md:px-8 py-0.5 md:py-1.5 transition-all`,
+                  numeroJaSorteado && `bg-slate-800`
+                )}
+              >
+                <p
+                  className={cn(
+                    "text-slate-950 text-center font-bold text-md md:text-xl transition-all",
+                    numeroJaSorteado && `text-slate-100`
+                  )}
+                >
+                  {(index + 1).toString().padStart(2, "0")}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col items-center border-2 border-slate-100 bg-emerald-200 px-4 md:px-6 py-2 md:py-4 md:gap-4 rounded-3xl md:rounded-4xl">
+            <p className="text-xl md:text-3xl font-bold text-center">Número:</p>
+            <p className="text-4xl md:text-7xl font-bold text-center">
+              {numeroSorteado?.toString().padStart(2, "0") ?? "--"}
+            </p>
+          </div>
+          <div className="border-2 bg-slate-50 border-slate-100 p-4 rounded-4xl h-84 md:h-96 flex flex-col">
+            <p className="text-sm md:text-lg underline font-bold text-center mb-4">
+              Números já sorteados
+            </p>
+            <div className="flex-col items-center md:gap-2 overflow-auto">
+              {numerosSorteados.length === 0 ? (
+                <p className="text-xs md:text-sm text-center italic text-slate-950">
+                  Nenhum número sorteado
+                </p>
+              ) : (
+                <div className="grid grid-cols-4 gap-1">
+                  {numerosSorteados.map((numero, index) => (
+                    <p
+                      key={`sorteado-${index}-${numero}`}
+                      className="text-sm text-center md:text-2xl font-bold text-slate-950"
+                    >
+                      {numero.toString().padStart(2, "0")}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="sticky bottom-0 bg-slate-300 border-t-2 border-t-slate-300 flex items-center justify-center py-6 gap-3">
+        <Button
+          onClick={sortearNumero}
+          disabled={numerosSorteados.length >= qtdeNumeros}
+          variant="default"
+          size="lg"
+        >
+          <DicesIcon className="size-4" />
+          <p>Sortear um número</p>
+        </Button>
+        <Button
+          onClick={confirmarReiniciarSorteio}
+          variant="destructive"
+          size="lg"
+        >
+          <RotateCcw className="size-4" />
+          <p>Reiniciar sorteio</p>
+        </Button>
+        <Input
+          className="w-24 border-slate-600"
+          placeholder="Qtde. números"
+          type="number"
+          min={MINIMO_QTDE_NUMEROS}
+          max={MAXIMO_QTDE_NUMEROS}
+          defaultValue={QTDE_PADRAO_NUMEROS}
+          onChange={(e) => setNovaQtdeNumeros(Number.parseInt(e.target.value))}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
