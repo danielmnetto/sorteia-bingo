a5c909b96e2896bb7f1380e0712f1df1ec4ea38a
Daniel Mendes Neto <danielm_netto@hotmail.com>
2025-11-14 00:13:13 -0300

    adicionar viva-voz, ajustar botões na tela e favicon novo

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,12 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { DicesIcon, RotateCcw } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 
-const MINIMO_QTDE_NUMEROS = 5;
-const MAXIMO_QTDE_NUMEROS = 300;
+const MINIMO_QTDE_NUMEROS = 2;
+const MAXIMO_QTDE_NUMEROS = 999;
 const QTDE_PADRAO_NUMEROS = 75;
 
 export default function Home() {
@@ -16,11 +19,23 @@ export default function Home() {
   const [novaQtdeNumeros, setNovaQtdeNumeros] =
     useState<number>(QTDE_PADRAO_NUMEROS);
   const [qtdeNumeros, setQtdeNumeros] = useState<number>(novaQtdeNumeros);
+  const [vivaVozAtivada, setVivaVozAtivada] = useState<boolean>(true);
 
   const pegarNumeroAleatorio = (minimo: number, maximo: number) => {
     minimo = Math.ceil(minimo);
     maximo = Math.floor(maximo);
     return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+  };
+
+  const falarNumeroVozAlta = (numero: number) => {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    }
+
+    const utterance = new SpeechSynthesisUtterance();
+
+    utterance.text = String(numero);
+    speechSynthesis.speak(utterance);
   };
 
   const sortearNumero = () => {
@@ -44,12 +59,22 @@ export default function Home() {
     }
     setNumeroSorteado(numeroSorteado);
     setNumerosSorteados([numeroSorteado, ...numerosSorteados]);
+    if (vivaVozAtivada) falarNumeroVozAlta(numeroSorteado);
   };
 
   const reiniciarSorteio = () => {
+    const qtdeNumerosAux = Math.max(Math.min(novaQtdeNumeros || QTDE_PADRAO_NUMEROS, MAXIMO_QTDE_NUMEROS), MINIMO_QTDE_NUMEROS);
+
     setNumeroSorteado(null);
     setNumerosSorteados([]);
-    setQtdeNumeros(novaQtdeNumeros || QTDE_PADRAO_NUMEROS);
+    setQtdeNumeros(qtdeNumerosAux);
+  };
+
+  const alternarVivaVoz = (estado: boolean) => {
+    if (speechSynthesis.speaking) {
+      speechSynthesis.cancel();
+    }
+    setVivaVozAtivada(estado);
   };
 
   const confirmarReiniciarSorteio = () => {
@@ -62,9 +87,11 @@ export default function Home() {
     reiniciarSorteio();
   };
 
+  const todosNumerosForamSorteados = numerosSorteados.length >= qtdeNumeros;
+
   return (
     <main className="relative flex flex-col">
-      <div className="flex justify-center gap-6 mx-auto my-2 px-5 py-4 border-2 border-slate-200 rounded-2xl">
+      <div className="flex justify-center gap-6 mx-auto mt-2 mb-4 px-5 py-4 border-2 border-slate-200 rounded-2xl">
         <div className="grid grid-cols-3 md:grid-cols-7 gap-1 md:gap-2 h-132 overflow-auto pr-3">
           {Array.from({ length: qtdeNumeros }).map((numero, index) => {
             const numeroJaSorteado = numerosSorteados.some(
@@ -92,13 +119,15 @@ export default function Home() {
           })}
         </div>
         <div className="flex flex-col gap-4">
-          <div className="flex flex-col items-center border-2 border-slate-100 bg-emerald-700 px-4 md:px-6 py-2 md:py-4 rounded-3xl md:rounded-4xl">
-            <p className="text-xl md:text-3xl font-bold text-center text-slate-50 underline">Número</p>
-            <p className="text-4xl md:text-8xl font-bold text-center text-slate-50">
+          <div className="flex flex-col items-center border-2 border-emerald-200 bg-emerald-700 px-4 md:px-6 py-2 md:py-4 rounded-3xl md:rounded-4xl shadow-lg">
+            <p className="text-lg md:text-xl font-bold text-center text-slate-50 underline">
+              Número da vez
+            </p>
+            <p className="text-4xl md:text-9xl font-bold text-center text-slate-50">
               {numeroSorteado?.toString().padStart(2, "0") ?? "--"}
             </p>
           </div>
-          <div className="border-2 bg-slate-50 border-slate-100 p-4 rounded-4xl h-84 flex flex-col">
+          <div className="border-2 bg-slate-100 border-slate-300 p-4 rounded-4xl h-84 flex flex-col shadow-lg">
             <p className="text-sm md:text-lg underline font-bold text-center mb-4">
               Números já sorteados
             </p>
@@ -123,33 +152,58 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="sticky bottom-0 bg-slate-300 border-t-2 border-t-slate-300 flex items-center justify-center py-6 gap-3">
-        <Button
-          onClick={sortearNumero}
-          disabled={numerosSorteados.length >= qtdeNumeros}
-          variant="default"
-          size="lg"
-        >
-          <DicesIcon className="size-4" />
-          <p>Sortear um número</p>
-        </Button>
-        <Input
-          className="w-24 border-slate-600"
-          placeholder="Qtde. números"
-          type="number"
-          min={MINIMO_QTDE_NUMEROS}
-          max={MAXIMO_QTDE_NUMEROS}
-          defaultValue={QTDE_PADRAO_NUMEROS}
-          onChange={(e) => setNovaQtdeNumeros(Number.parseInt(e.target.value))}
-        />
-        <Button
-          onClick={confirmarReiniciarSorteio}
-          variant="destructive"
-          size="lg"
-        >
-          <RotateCcw className="size-4" />
-          <p>Reiniciar sorteio</p>
-        </Button>
+      <div className="sticky bottom-0 bg-slate-300 border-t-2 border-t-slate-300 flex flex-col items-center justify-center py-6 gap-3">
+        <div className="flex gap-2 justify-center">
+          <Button
+            onClick={sortearNumero}
+            disabled={todosNumerosForamSorteados}
+            variant="default"
+            size="xl"
+          >
+            <DicesIcon className="size-4" />
+            <p>
+              {todosNumerosForamSorteados
+                ? "Todos os números foram sorteados"
+                : "Sortear um número"}
+            </p>
+          </Button>
+        </div>
+        <Separator className="bg-slate-400" />
+        <div className="flex gap-2 justify-center">
+          <div className="flex gap-2 items-center">
+            <Label htmlFor="qtde-numeros">Qtde. Números</Label>
+            <Input
+              id="qtde-numeros"
+              className="w-24 border-slate-600"
+              placeholder="Qtde. números"
+              type="number"
+              min={MINIMO_QTDE_NUMEROS}
+              max={MAXIMO_QTDE_NUMEROS}
+              defaultValue={QTDE_PADRAO_NUMEROS}
+              onChange={(e) =>
+                setNovaQtdeNumeros(Number.parseInt(e.target.value))
+              }
+            />
+          </div>
+          <Separator className="bg-slate-400" orientation="vertical" />
+          <div className="flex gap-1 items-center">
+            <Switch
+              id="ativar-viva-voz"
+              checked={vivaVozAtivada}
+              onCheckedChange={(checked) => alternarVivaVoz(checked)}
+            />
+            <Label htmlFor="ativar-viva-voz">Viva-voz</Label>
+          </div>
+          <Separator className="bg-slate-400" orientation="vertical" />
+          <Button
+            onClick={confirmarReiniciarSorteio}
+            variant="destructive"
+            size="lg"
+          >
+            <RotateCcw className="size-4" />
+            <p>Reiniciar sorteio</p>
+          </Button>
+        </div>
       </div>
     </main>
   );
